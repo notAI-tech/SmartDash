565392b755389402d97792a1b7d4264b7bb4751d
Bedapudi Praneeth <praneeth@bpraneeth.com>
2023-10-25 15:23:32 +0530

    Update setup.py

--- smartlog/setup.py
+++ smartlog/setup.py
@@ -18,10 +18,10 @@ URL = "https://github.com/notAI-tech/smartdash"
 EMAIL = "praneeth@bpraneeth.com"
 AUTHOR = "BEDAPUDI PRANEETH"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.1.dev21"
+VERSION = "0.0.1.dev22"
 
 # What packages are required for this module to be executed?
-REQUIRED = ["requests", "liteindex"]
+REQUIRED = ["requests", "liteindex==0.0.1.dev25"]
 
 # What packages are optional?
 EXTRAS = {
